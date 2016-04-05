668809037f1b5cf6dcb8387a240620e2a8d669f7
bjahng <bjj34@yahoo.com>
2016-04-05 13:32:55 -0700

    noninteractive version

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 $(function(){
 
+	var state = 0;
+
+	$(".box").click(function() {
+		
+		if ( $(this).hasClass("ex") || $(this).hasClass("oh") ) {
+			event.preventDefault();
+		}
+		else if ( state == 0 ) {
+			$(this).addClass("ex");
+			state = 1;
+		}
+		else {
+			$(this).addClass("oh");
+			state = 0;
+		}
+	});
+
+	$(".new_game").click(function() {
+		$(".box").removeClass("ex");
+		$(".box").removeClass("oh");
+	});
+
+
 });
