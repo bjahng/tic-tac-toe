6dd3482f4ec647c52583b3c509b58c65be99060a
bjahng <bjj34@yahoo.com>
2016-04-07 08:28:42 -0700

    almost done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(function(){
 
 	var state = 0;
-
+	
 	$(".box").click(function() {
-		
+
 		if ( $(this).hasClass("ex") || $(this).hasClass("oh") ) {
 			event.preventDefault();
 		}
@@ -15,12 +15,44 @@ $(function(){
 			$(this).addClass("oh");
 			state = 0;
 		}
+		
+		win_checker();
 	});
+
+	var win_checker = function() {
+
+		var winners = [
+		[".r1.c1", ".r1.c2", ".r1.c3"],
+		[".r2.c1", ".r2.c2", ".r2.c3"],
+		[".r3.c1", ".r3.c2", ".r3.c3"],
+		[".r1.c1", ".r2.c1", ".r3.c1"],
+		[".r1.c2", ".r2.c2", ".r3.c2"],
+		[".r1.c3", ".r2.c3", ".r3.c3"],
+		[".r1.c1", ".r2.c2", ".r3.c3"],
+		[".r1.c3", ".r2.c2", ".r3.c1"]
+		];
+
+		for ( x=0; x < winners.length; x++) {
+			if ( $(winners[x][0]).hasClass("ex") && $(winners[x][1]).hasClass("ex") && $(winners[x][2]).hasClass("ex") ) {
+				$(".results").prepend("<p>Player X Wins!</p>");
+				return false;
+			}
+			else if ( $(winners[x][0]).hasClass("oh") && $(winners[x][1]).hasClass("oh") && $(winners[x][2]).hasClass("oh") ) {
+				$(".results").prepend("<p>Player O Wins!</p>");
+				return false;
+			}
+			else if ( $(".ex").length + $(".oh").length == 9 ) {
+				$(".results").prepend("<p>It's a tie!</p>");
+				return false;
+			}
+		}
+	}
+
 
 	$(".new_game").click(function() {
 		$(".box").removeClass("ex");
 		$(".box").removeClass("oh");
+		$(".results").children().remove("p");
 	});
-
 
 });
